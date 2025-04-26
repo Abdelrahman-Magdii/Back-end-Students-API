0313e8fce68aa3500948272aac5b1bc8511e48d9
Abdelrahman-Magdii <abdulrahman403828@fci.bu.edu>
2025-04-27 00:16:09 +0300

    github actions

--- src/main/java/com/spring/student/config/SpringSecurity.java
+++ src/main/java/com/spring/student/config/SpringSecurity.java
@@ -9,6 +9,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,6 +18,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -40,24 +46,37 @@ public class SpringSecurity {
         return new InMemoryUserDetailsManager(admin);
     }
 
+    public static final String[] PUBLIC_APIS ={
+            "/swagger-ui/**","/v3/api.docs/**","/auth","/signin"
+    };
     // Configure HttpSecurity with JWT filters and session management
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationManager authenticationManager) throws Exception {
         http
-            .csrf().disable()
+            .cors(cors -> cors.configurationSource(corsConfigurationSource())) // Enable CORS
+            .csrf(AbstractHttpConfigurer::disable) // Disable CSRF for WebSockets
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .addFilter(new JwtAuthenticationFilter(authenticationManager))
             .addFilter(new JwtAuthorizationFilter(authenticationManager))
             .authorizeRequests()
-            .requestMatchers(HttpMethod.POST, "/auth").permitAll()
-            .requestMatchers(HttpMethod.POST, "/signin").permitAll()
             .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-            .requestMatchers(HttpMethod.GET,"/swagger-ui/**", "/v3/api.docs/**").permitAll()
-            .anyRequest().authenticated()
-            .and()
-            .httpBasic();
+            .requestMatchers(PUBLIC_APIS).permitAll()
+            .anyRequest().authenticated();
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        configuration.setExposedHeaders(List.of("Authorization"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     // PasswordEncoder bean for password hashing
